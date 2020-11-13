4b58548ef7adac3b300087e51afd9ee7f542b5f6
ObviouslyLuuk <321luuk@live.nl>
2020-11-14 00:08:35 +0100

    renamed html to index.html so it can be published on github; made two separate JS files for the neural network with and without dead code; wrote a function in main.js to test many times and update time sums on the html page

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import {Network} from "./neural_network.js"
+import {Network as Network_DC} from "./neural_network_with_dead_code.js"
 
 // XOR with 3 inputs
 const xor_3 = [
@@ -12,10 +13,48 @@ const xor_3 = [
     {"x": [1,1,1], "y": [1,0]},        
 ]
 
-function initNet() {
+function initNet(dead_code=false) {
     let training_data = xor_3
-    let net = new Network(training_data, [10])
-    net.SGD(100000, 1, 0)
+    switch(dead_code) {
+        case false:
+            var net = new Network(training_data, [10])
+            break
+        case true:
+            var net = new Network_DC(training_data, [10])
+    }
+    net.SGD(1000, 1, -1)
 }
 
-initNet()
+function testNets(round, time_without, time_with) {
+    let time_sum = 0
+    let time_sum_dead_code = 0
+    let i = 0
+
+    function runTests() {
+        for (let dead_code of [false, true]) {
+            let t0 = performance.now()
+            initNet(dead_code)
+            let t1 = performance.now()
+
+            switch(dead_code) {
+                case false:
+                    time_sum += (t1-t0)
+                    break
+                case true:
+                    time_sum_dead_code += (t1-t0)
+            }
+        }
+        console.log("rounds finished")
+        round.innerHTML = i + 1
+        time_without.innerHTML = time_sum
+        time_with.innerHTML = time_sum_dead_code  
+
+        if (i < 5000) {
+            i++
+            setTimeout(runTests, 1)
+        }
+    }
+    runTests()
+}
+
+testNets(round, time_without, time_with)
